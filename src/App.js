76831cb7da9099571e6fd1c32ad5ff1c9199039d
Lauren Durr <laurenashdurr@gmail.com>
2021-07-03 10:50:01 -0400

    Got the test  Nasa component to display to the dom and console

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
 import TmComponent from './Components/ticketmaster'
+import Nasa from './Components/nasa'
 
 function App() {
   return (
     <div className="App">
       <TmComponent />
+      <Nasa />
     </div>
   );
 }
