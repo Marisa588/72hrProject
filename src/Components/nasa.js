dd0c380e76c78ba0c63d44d90e6852647c79ea3b
Lauren Durr <laurenashdurr@gmail.com>
2021-07-03 13:45:31 -0400

    Working on hitting the Nasa imagery endpoint with no luck..

--- src/Components/nasa.js
+++ src/Components/nasa.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
-const Nasa = () => {
 
-    console.log("did this work?")
+const Nasa = () => {
+    
+    fetch("https://api.nasa.gov/planetary/earth/assets?lon=-95.33&lat=29.78&date=2018-01-01&&dim=0.10&api_key=GhRyAVIU6OMKX2g6Odn1NkUpGWNkFLQenfVcKR4f") // this is currently calling to /assets endpoint when it should be "imagery". When I change to the "imagery" endpoint, I get an error. Not sure why
+    .then(res => res.json())
+    .then(data=>console.log(data))
+  
 
     return(
         <div>
-            <h1>Testing Testing</h1>
+            <h1>Testing Nasa</h1>
         </div>
     )
     
