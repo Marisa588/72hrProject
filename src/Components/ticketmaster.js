e7f43ac91bb5e79bbb8e6c2d868f7275bf9b221e
Dean Howell <howelldean81@gmail.com>
2021-07-02 21:03:39 -0400

    Push?

--- src/Components/ticketmaster.js
+++ src/Components/ticketmaster.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import {
-    Table,
+    Table
 } from 'reactstrap'
 
 let tmApiKey = "c7AGPZOyCV1Y4tUM7Nfa1OirwIRKx0VD"
 
-const tmComponent = () => {
+const tmComponent = (props) => {
     console.log(tmApiKey)
     return (
         <div>
             <h1>This is a test!</h1>
             <Table dark>
-                
+
             </Table>
         </div>
 
