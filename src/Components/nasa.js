042749dfde35047391c9f82733dc8594cdef1a46
Lauren Durr <laurenashdurr@gmail.com>
2021-07-03 18:35:08 -0400

    trying to figure out useState, but I ended up hitting my api limit for the day

--- src/Components/nasa.js
+++ src/Components/nasa.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+const baseURL = 'https://api.nasa.gov/planetary/earth/assets?'
+const key = 'GhRyAVIU6OMKX2g6Odn1NkUpGWNkFLQenfVcKR4f'
 
 
 const Nasa = () => {
     
-    fetch("https://api.nasa.gov/planetary/earth/assets?lon=-95.33&lat=29.78&date=2018-01-01&&dim=0.10&api_key=GhRyAVIU6OMKX2g6Odn1NkUpGWNkFLQenfVcKR4f") // this is currently calling to /assets endpoint when it should be "imagery". When I change to the "imagery" endpoint, I get an error. Not sure why
+    const [ nasaImg, setNasaImg] = useState([]);
+
+    //eventually lon and lat will be inputs
+    //useState is making too many calls 
+
+    fetch(baseURL + "lon=-95.33&lat=29.78&date=2018-01-01&&dim=0.10&api_key=" + key)
     .then(res => res.json())
-    .then(data=>console.log(data))
-  
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
 
     return(
         <div>
             <h1>Testing Nasa</h1>
+            <img src={''} />
         </div>
     )
     
