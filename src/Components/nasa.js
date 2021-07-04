5bac91f4e67a4babb84804e34fd2c4b124452053
Lauren Durr <laurenashdurr@gmail.com>
2021-07-04 17:06:41 -0400

    Updated endpoint for NASA API to /imagery and converted response to a URL link

--- src/Components/nasa.js
+++ src/Components/nasa.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Button } from 'reactstrap'
 
-const baseURL = 'https://api.nasa.gov/planetary/earth/assets?'
+const baseURL = 'https://api.nasa.gov/planetary/earth/imagery?'
 const key = 'GhRyAVIU6OMKX2g6Odn1NkUpGWNkFLQenfVcKR4f'
 
 
@@ -9,7 +9,7 @@ const Nasa = () => {
 
     const [lat, setLat] = useState('lat')
     const [lng, setLng] = useState('lng')
-    const [ nasaImg, setNasaImg] = useState(null);
+    const [nasaImg, setNasaImg] = useState(null);
 
     navigator.geolocation.getCurrentPosition((position) => {
         setLat(position.coords.latitude)
@@ -18,12 +18,14 @@ const Nasa = () => {
     })
 
     useEffect(() => {
-        fetch(baseURL + "lon=" + lng + "&lat=" + lat + "&date=2021-06-01&&dim=0.10&api_key=" + key)
-            .then(res => res.json())
-            .then(data => setNasaImg(data.url))
+        fetch(baseURL + "lon=" + lng + "&lat=" + lat + "&date=2021-06-01&&dim=.5&api_key=" + key)
+            .then(res => res.blob())
+            .then(function (myBlob) {
+                setNasaImg(URL.createObjectURL(myBlob))
+            })
             .catch(err => console.log(err));
 
-    }, [setLng])
+    }, [])
 
 
     // const [ nasaImg, setNasaImg] = useState(null);
