68143c0b9e825302476501422a6d7041b735e74c
Lauren Durr <laurenashdurr@gmail.com>
2021-07-04 16:31:57 -0400

    Not working 100% of the time, but I think this is headed in the right direction at least

--- src/Components/nasa.js
+++ src/Components/nasa.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from 'reactstrap'
 
 const baseURL = 'https://api.nasa.gov/planetary/earth/assets?'
@@ -6,29 +6,61 @@ const key = 'GhRyAVIU6OMKX2g6Odn1NkUpGWNkFLQenfVcKR4f'
 
 
 const Nasa = () => {
-    
+
+    const [lat, setLat] = useState('lat')
+    const [lng, setLng] = useState('lng')
     const [ nasaImg, setNasaImg] = useState(null);
 
-    const fetchNasa = () => {
-    fetch(baseURL + "lon=-95.33&lat=29.78&date=2018-01-01&&dim=0.10&api_key=" + key)
-    .then(res => res.json())
-    .then(data => setNasaImg(data.url))
-    .catch(err => console.log(err));
-    };
+    navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude)
+        setLng(position.coords.longitude)
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        fetchNasa();
-    }
+    })
 
-    return(
+    useEffect(() => {
+        fetch(baseURL + "lon=" + lng + "&lat=" + lat + "&date=2021-06-01&&dim=0.10&api_key=" + key)
+            .then(res => res.json())
+            .then(data => setNasaImg(data.url))
+            .catch(err => console.log(err));
+
+    }, [setLng])
+
+
+    // const [ nasaImg, setNasaImg] = useState(null);
+
+    // useEffect(() => {
+
+    //     navigator.geolocation.getCurrentPosition((position) => {
+    //         let lat = position.coords.latitude
+    //         let lng = position.coords.longitude
+
+    //         console.log(lat)
+    //         console.log(lng)
+    //     })
+
+    // }, [])
+
+
+    // const fetchNasa = () => {
+    // fetch(baseURL + "lon=-95.33&lat=29.78&date=2018-01-01&&dim=0.10&api_key=" + key)
+    // .then(res => res.json())
+    // .then(data => setNasaImg(data.url))
+    // .catch(err => console.log(err));
+    // };
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     fetchNasa();
+    // }
+
+    return (
         <div>
             <h1>Testing Nasa</h1>
-            <Button onClick={handleSubmit}>Handle Submit</Button>
-            <img src={nasaImg}/>
+            {/* <Button onClick={handleSubmit}>Handle Submit</Button> */}
+            <img src={nasaImg} />
         </div>
     )
-    
+
 }
 
-export default Nasa; 
+export default Nasa;
