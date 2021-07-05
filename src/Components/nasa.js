fedcaf94a9d2191a5259814ed1a380331ab7caed
Lauren Durr <laurenashdurr@gmail.com>
2021-07-05 15:48:19 -0400

    added geolocation to App.js in order to grab location on load of application. Implemented lat and long via props into Nasa component.

--- src/Components/nasa.js
+++ src/Components/nasa.js
@@ -1,36 +1,28 @@
 import React, { useState, useEffect } from 'react'
-import { Button } from 'reactstrap'
+
 
 const baseURL = 'https://api.nasa.gov/planetary/earth/imagery?'
 const key = 'GhRyAVIU6OMKX2g6Odn1NkUpGWNkFLQenfVcKR4f'
 
 
-const Nasa = () => {
+const Nasa = (props) => {
 
-    const [lat, setLat] = useState('lat')
-    const [lng, setLng] = useState('lng')
     const [nasaImg, setNasaImg] = useState(null);
 
-    navigator.geolocation.getCurrentPosition((position) => {
-        setLat(position.coords.latitude)
-        setLng(position.coords.longitude)
-
-    })
-
     useEffect(() => {
-        fetch(baseURL + "lon=" + lng + "&lat=" + lat + "&date=2021-06-01&&dim=.5&api_key=" + key)
+        fetch(baseURL + "lon=" + props.lng + "&lat=" + props.lat + "&date=2021-06-01&&dim=.15&api_key=" + key)
             .then(res => res.blob())
             .then(function (myBlob) {
                 setNasaImg(URL.createObjectURL(myBlob))
             })
             .catch(err => console.log(err));
 
-    }, [])
+    }, [setNasaImg])
 
     return (
         <div>
             <h1>Testing Nasa</h1>
-            <h1>{lat}, {lng}</h1>
+            <h1>{props.lat}, {props.lng}</h1>
             <img alt="Overhead shot of location based on current lat and long" src={nasaImg} />
         </div>
     )
