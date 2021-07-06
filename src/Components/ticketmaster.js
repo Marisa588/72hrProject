f6c4eb403b56f29f9307614465357c1e9d036bb1
Dean Howell <howelldean81@gmail.com>
2021-07-06 00:38:44 -0400

    Added table and table styling. Preliminary .map that doesn't even work.

--- src/Components/ticketmaster.js
+++ src/Components/ticketmaster.js
@@ -1,36 +1,47 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
-    Table,
-    Button
+    Table
 } from 'reactstrap'
+
+const tmRootUrl = "https://app.ticketmaster.com/discovery/v2/events.json?"
+const tmApiKey = "c7AGPZOyCV1Y4tUM7Nfa1OirwIRKx0VD"
 
 const TmComponent = (props) => {
 
     useEffect(() => {
-        // API vars
-        const tmRootUrl = "https://app.ticketmaster.com/discovery/v2/"
-        const tmApiKey = "c7AGPZOyCV1Y4tUM7Nfa1OirwIRKx0VD"
-        console.log(tmApiKey)
+        async function getEvents() {
+            try {
+                const response = await fetch(
+                    `${tmRootUrl}latlong=${props.lat},${props.lng}&radius=50&unit=miles&apikey=${tmApiKey}`
+                )
+                const json = await response.json()
+                const data = json._embedded.events
+                console.log({ data })
+            } catch (error) { }
+        }
+        getEvents()
+    })
 
-        navigator.geolocation.getCurrentPosition((position) => {
-            let lat = position.coords.latitude
-            let lng = position.coords.longitude
-
-            console.log(lat)
-            console.log(lng)
-        })
-        
-    }, [])
-
-    // const getEvents = () => {
-    //     async function
-    // }
-    
     return (
+        
         <div>
             <h1>This is a test!</h1>
             <Table dark>
-
+                <thead>
+                    <tr>
+                        <th>Event Name</th>
+                        <th>Date</th>
+                        <th>Distance</th>
+                    </tr>    
+                </thead>
+                return ()
+                {data.length > 0 && data.map((eventData, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{eventData.name}</td>
+                        </tr>
+                    )
+                })}
             </Table>
         </div>
 
